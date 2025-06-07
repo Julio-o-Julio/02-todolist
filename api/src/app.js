1e2a01b6637a863188f8fc556742d657c4013536
Julio Souza <julioamsf@gmail.com>
2025-06-07 16:06:58 -0400

    feat: Feita a implementação da rota GET /logs que consome a tabela Log do banco de dados, mostrando todos os logs de alterações no banco de dados

--- api/src/app.js
+++ api/src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const todoRoutes = require('./routes/todoRoutes');
 const tagRoutes = require('./routes/tagRoutes');
+const logRoutes = require('./routes/logRoutes');
 
 const app = express();
 
@@ -9,6 +10,7 @@ app.use(express.json());
 app.use(cors());
 app.use(todoRoutes);
 app.use(tagRoutes);
+app.use(logRoutes);
 
 app.get('/', (request, response) => {
   return response.json('up');
