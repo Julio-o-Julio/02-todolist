976d7230aa0a48c67f251845f723dbddb751fbf8
Julio Souza <julioamsf@gmail.com>
2025-06-09 01:15:01 -0400

    bug: Arrumando bug no index.js

--- api/src/index.js
+++ api/src/index.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
   console.log(`Server up in route: ${PORT}`);
-
   const endpoints = expressListEndpoints(app);
   console.table(endpoints);
 });
